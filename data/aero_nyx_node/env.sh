be384bac04c682a9db6c7c01d6cedb205c7d20d0
root <hi@aeronyx.network>
2024-07-08 11:34:10 +0800

    Fix tun bugs

--- data/aero_nyx_node/env.sh
+++ data/aero_nyx_node/env.sh
@@ -1,4 +1,4 @@
 
 echo 1 | tee /proc/sys/net/ipv4/ip_forward
 iptables -P FORWARD ACCEPT   
-iptables -t nat -I POSTROUTING -s 192.168.50.0/255.255.255.0 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 192.168.0.0/255.255.0.0 -o eth0 -j MASQUERADE
